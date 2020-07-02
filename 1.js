f016f63c41553ea4bbe564fa441b390f9c051162
Yuan Yong <yybug@163.com>
2020-07-02 09:41:52 +0800

    https

--- 1.js
+++ 1.js
@@ -1,15 +1,38 @@
-var http = require('http');
+//使用nodejs自带的http、https模块
+const https = require('https');
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const koa = require('koa');
+const app = new koa();
 
-http.createServer(function (request, response) {
+app.on('error', (error, ctx) => {
+    console.log('something error ' + JSON.stringify(ctx.onerror));
+});
 
-    // 发送 HTTP 头部 
-    // HTTP 状态值: 200 : OK
-    // 内容类型: text/plain
-    response.writeHead(200, {'Content-Type': 'text/plain'});
+app.use(async ctx => {
+    ctx.body = `This is ${ctx.protocol} visit`;
+});
 
-    // 发送响应数据 "Hello World"
-    response.end('Hello World\n');
-}).listen(8888);
+//根据项目的路径导入生成的证书文件
+const privateKey  = fs.readFileSync(path.join(__dirname, './certificate/server.key'), 'utf8');
+const certificate = fs.readFileSync(path.join(__dirname, './certificate/server.crt'), 'utf8');
+const credentials = {key: privateKey, cert: certificate};
 
-// 终端打印如下信息
-console.log('Server running at http://127.0.0.1:8888/');
+//创建http与HTTPS服务器
+const httpServer = http.createServer(app.callback());
+const httpsServer = https.createServer(credentials, app.callback());
+
+//可以分别设置http、https的访问端口号
+const PORT = 8000;
+const SSLPORT = 8001;
+
+//创建http服务器
+httpServer.listen(PORT, function() {
+    console.log('HTTP Server is running on: http://localhost:%s', PORT);
+});
+
+//创建https服务器
+httpsServer.listen(SSLPORT, function() {
+    console.log('HTTPS Server is running on: https://localhost:%s', SSLPORT);
+});
